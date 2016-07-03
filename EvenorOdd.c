043dd415a9f3150855c3c11ffc1adc328c9b9ebe
Kishorepm <kishorepmofficial@gmail.com>
2016-07-03 21:55:46 +0530

    Add files via upload

---
+++ EvenorOdd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("Enter a number : ");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("The given number is even");
+}
+else
+{
+printf("The given number is odd");
+}
+}
