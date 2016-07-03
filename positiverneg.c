096c96875c909d9ac115fc4614cba1840d9d1ae8
Kishorepm <kishorepmofficial@gmail.com>
2016-07-03 21:38:24 +0530

    Add files via upload

---
+++ positiverneg.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+int a;
+printf("Enter any number : ");
+scanf("%d",&a);
+if(a==0)
+{
+printf("The entered number is equal to zero");
+}
+else if(a>0)
+{
+printf("%d is a positive number",a);
+}
+else
+{
+printf("%d is a negative number ",a);
+}
+}
