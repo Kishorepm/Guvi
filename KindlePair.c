0766c582d661299a76991944b6704c5942221fdc
Kishorepm <kishorepmofficial@gmail.com>
2016-07-10 14:47:07 +0530

    Add files via upload

---
+++ KindlePair.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+int n,p,i,j,count=1;
+static int c=1;
+printf("Enter the number of students:");
+scanf("%d",&n);
+p=n/2;
+printf("The number of Pairs:%d",p);
+int a[n];
+for(i=1;i<=n-1;i++)
+{
+    for(j=1;j<2;j++)
+    {
+        if(count<=n)
+        {
+         a[i][j]=count;
+         count++;
+        }
+        else
+        {
+         a[i][j]=c;
+         c++;
+        }
+    }
+}
+for(i=1;i<=n-1;i++)
+{
+    for(j=1;j<2;j++)
+    {
+        printf("The Students in pairs will b:%d",a[i][j]);
+    }
+}
+return 0;
+}
